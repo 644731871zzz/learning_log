765c95a0d1d27b6611d1e6d3b87f0d2b04e33f87
644731871zzz <644731871zzz@gmail.com>
2025-03-11 23:18:38 +0800

    03112318

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,10 +2,14 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name='accounts'
 urlpatterns=[
     #包含身份验证的urls 这是django自带的身份认真
     #自带功能编入acounts下后,只能用这里的app的url访问
     #django的默认视图中有login  所以导向这里会继续寻找这个urls中的函数
     path('',include('django.contrib.auth.urls')),
+    #注册界面
+    path('register/', views.register,name='register')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def register(request):
+    """注册新用户"""
+    if request.method != 'POST':
+        #显示空的注册表单
+        form = UserCreationForm()
+    else:
+        #根据用户数据处理填写好的表单
+        form = UserCreationForm(data=request.POST)
+        #检查数据是否有效(没有非法字符,名称重复,恶意名称,确认密码是否相同等)
+        if form.is_valid():
+            new_user = form.save() #存储用户名,密码的哈希值到数据库,赋值到new_user上
+            #让用户自动登录,再重新定向
+            login(request,new_user)
+            return redirect('learning_logs:index')
+        
+    #显示空表单或指出表单无效
+    #如果用户填写的信息无效,is_valid拒绝了,但是其中的form还包含无效的信息传递给模板显示
+    context = {'form': form}
+    return render(request,'registration/register.html',context)
