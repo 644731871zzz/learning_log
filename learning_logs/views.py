f071c401e659d6a9b539bb2cd9a5adec2d7fa7a5
644731871zzz <644731871zzz@gmail.com>
2025-03-16 23:02:09 +0800

    03162301

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic,Entry
 
@@ -12,8 +13,8 @@ def index(request):
 @login_required
 def topics(request):
     """显示所有的主题"""
+    #依照已经登录的用户显示
     topics = Topic.objects.filter(owner=request.user).order_by('date_added')
-    topics = Topic.objects.order_by('date_added')
     context = {'topics':topics}
     return render(request,'learning_logs/topics.html',context)
 
@@ -21,6 +22,11 @@ def topics(request):
 def topic(request,topic_id):
     """显示单个主题极其所有的条目"""
     topic=Topic.objects.get(id=topic_id)
+
+    #确定请求主题的用户是否关联此主题
+    #导入练习19.3定义的函数进行验证
+    check_topic_owner(request,topic)
+    
     entries=topic.entry_set.order_by('-date_added')
     context={'topic':topic,'entries':entries}
     return render(request,'learning_logs/topic.html',context)
@@ -39,7 +45,11 @@ def new_topic(request):
         #一些字段的要求是写在models中,将会自动验证
         #字段都有效将会调用save
         if form.is_valid():
-            form.save()
+            #不立即保存到数据库,先创建表单,但是不提交,仅一次访问数据库
+            new_topic = form.save(commit=False)
+            new_topic.owner =request.user
+            new_topic.save()
+            
             #将用户界面重定向跳转
             return redirect('learning_logs:topics')
         
@@ -82,6 +92,9 @@ def edit_entry(request,entry_id):
     entry=Entry.objects.get(id=entry_id)
     topic=entry.topic
 
+    #导入练习19.3定义的函数进行验证
+    check_topic_owner(request,topic)
+
     if request.method !='POST':
         #初次请求将使用当前的条目填成标签
         form=EntryForm(instance=entry)
@@ -94,3 +107,14 @@ def edit_entry(request,entry_id):
         
     context={'entry':entry,'topic':topic,'form':form}
     return render(request,'learning_logs/edit_entry.html',context)
+
+
+
+def check_topic_owner(request,topic):
+    """练习19.3作业,用于验证用户id和配置主题id是否匹配."""
+    if topic.owner != request.user:
+        #raise直接终止视图返回错误界面 
+        # return redirect可能会暴露entry_id是否存在
+        # 因为仅仅是跳转,可能知道检查user的函数运行了,id就是访问的id
+        raise Http404
+
